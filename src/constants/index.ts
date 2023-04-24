b9a4cb8e082b9d9a4dcc260363d5fe6973f48cb6
furyeyes <ioannis.aarde@gmail.com>
2023-04-24 13:00:45 +0200

    mainnet program ids

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,43 +9,41 @@ const MAINNET_RPC_ENDPOINT = config.parsed
 // verified creator of the HGB collection
 export const verifiedCreator = "6vRx1iVZo3xfrBHdpvuwArL2jucVj9j9nLpd2VUTTGMG";
 export const HONEY_PROGRAM_ID = new PublicKey(
-  "AoaqbAiwMVK12MQHkRi7p5aemc1CQ271JyuyeHzXonXu"
+  "hNEYyRsRBVq2La65V1KjvdbTE39w36gwrdjkmcpvysk"
 );
 // all the id's of the markets
 export const HONEY_MARKET_IDS = [
-  new PublicKey("ACv7BxtW7QzvFyLtmxtB8xZBQrfHAyTAjdCZ3q4QEowF"),
-  // new PublicKey("6FcJaAzQnuoA6o3sVw1GD6Ba69XuL5jinZpQTzJhd2R3"), // Honey Gen
-  // new PublicKey("Bw77MGpg189EaATjN67WXcnp3c4544LhKoV4Ftmdg4C"), // Pesky Penguins
-  // new PublicKey("H2H2pJuccdvpET9A75ajB3GgdYdCUL4T3kiwUMA6DJ7q"), // Lifinity flares
-  // new PublicKey("Bxk1JQCbVjpeFnjzvH5n9bepnZeHjRADUFwZiVC7L5Gq"), // OG Atadians
-  // new PublicKey("F8rZviSSuqgkTsjMeoyrTUSNSqh7yNDCAozJkxm7eujY"), // Burrito
-  // new PublicKey("GrKPvcdHVb4cwR5a2CCgCTvdkSqhNDRgSUiUVzXRWLk6"), // Blocksmith
-  // new PublicKey("5UKRRSxbi4PgPnQU2ZqtukUxd1fyN6ydn1hoxivP46A8"), // Ovols
-  // new PublicKey("GAqyPziKPwVpwKaeqPhEsxiy6MwQ2bvtodruWErpLVKo"), // Droids
-  // new PublicKey("2dxJ4eMkhMxm1ZqpAhKsjunvyziuq1JRnuHaqKFRY8et"), // Vandals
-  // new PublicKey("FTBLaLcrx1aXALW2UEpu8a6HLRVFATezkK12wCABPAiA"), // Ukiyo
-  // new PublicKey("Dmngi1MDEQU9fm6sX39EuyT3EpYEmXYuyg56uEjVCkD6"), // Marshies
-  // new PublicKey("5ZxAjKpbYje5fCxhvnRYxbMh6XSZm5Cd7RA9mMGb1DLY"), // Heavenland
-  // new PublicKey("7pfaZcAqpWRHpEqGMwPQrn5tj5WVQ48F4PrAtFLuS1P7"), // Drunken Ape Social Club
-  // new PublicKey("HyUDgtmrERNC6xnPUjxs7fvkB6rX5esqvP5LW4LiXzrV"), // Wolf capital
+  new PublicKey("6FcJaAzQnuoA6o3sVw1GD6Ba69XuL5jinZpQTzJhd2R3"), // Honey Gen
+  new PublicKey("Bw77MGpg189EaATjN67WXcnp3c4544LhKoV4Ftmdg4C"), // Pesky Penguins
+  new PublicKey("H2H2pJuccdvpET9A75ajB3GgdYdCUL4T3kiwUMA6DJ7q"), // Lifinity flares
+  new PublicKey("Bxk1JQCbVjpeFnjzvH5n9bepnZeHjRADUFwZiVC7L5Gq"), // OG Atadians
+  new PublicKey("F8rZviSSuqgkTsjMeoyrTUSNSqh7yNDCAozJkxm7eujY"), // Burrito
+  new PublicKey("GrKPvcdHVb4cwR5a2CCgCTvdkSqhNDRgSUiUVzXRWLk6"), // Blocksmith
+  new PublicKey("5UKRRSxbi4PgPnQU2ZqtukUxd1fyN6ydn1hoxivP46A8"), // Ovols
+  new PublicKey("GAqyPziKPwVpwKaeqPhEsxiy6MwQ2bvtodruWErpLVKo"), // Droids
+  new PublicKey("2dxJ4eMkhMxm1ZqpAhKsjunvyziuq1JRnuHaqKFRY8et"), // Vandals
+  new PublicKey("FTBLaLcrx1aXALW2UEpu8a6HLRVFATezkK12wCABPAiA"), // Ukiyo
+  new PublicKey("Dmngi1MDEQU9fm6sX39EuyT3EpYEmXYuyg56uEjVCkD6"), // Marshies
+  new PublicKey("5ZxAjKpbYje5fCxhvnRYxbMh6XSZm5Cd7RA9mMGb1DLY"), // Heavenland
+  new PublicKey("7pfaZcAqpWRHpEqGMwPQrn5tj5WVQ48F4PrAtFLuS1P7"), // Drunken Ape Social Club
+  new PublicKey("HyUDgtmrERNC6xnPUjxs7fvkB6rX5esqvP5LW4LiXzrV"), // Wolf capital
 ];
 
 export const MARKET_IDS_STRING: string[] = [
-  "ACv7BxtW7QzvFyLtmxtB8xZBQrfHAyTAjdCZ3q4QEowF",
-  // "6FcJaAzQnuoA6o3sVw1GD6Ba69XuL5jinZpQTzJhd2R3",
-  // "Bw77MGpg189EaATjN67WXcnp3c4544LhKoV4Ftmdg4C",
-  // "H2H2pJuccdvpET9A75ajB3GgdYdCUL4T3kiwUMA6DJ7q",
-  // "Bxk1JQCbVjpeFnjzvH5n9bepnZeHjRADUFwZiVC7L5Gq",
-  // "F8rZviSSuqgkTsjMeoyrTUSNSqh7yNDCAozJkxm7eujY",
-  // "GrKPvcdHVb4cwR5a2CCgCTvdkSqhNDRgSUiUVzXRWLk6",
-  // "5UKRRSxbi4PgPnQU2ZqtukUxd1fyN6ydn1hoxivP46A8",
-  // "GAqyPziKPwVpwKaeqPhEsxiy6MwQ2bvtodruWErpLVKo",
-  // "2dxJ4eMkhMxm1ZqpAhKsjunvyziuq1JRnuHaqKFRY8et",
-  // "FTBLaLcrx1aXALW2UEpu8a6HLRVFATezkK12wCABPAiA",
-  // "Dmngi1MDEQU9fm6sX39EuyT3EpYEmXYuyg56uEjVCkD6",
-  // "5ZxAjKpbYje5fCxhvnRYxbMh6XSZm5Cd7RA9mMGb1DLY",
-  // "7pfaZcAqpWRHpEqGMwPQrn5tj5WVQ48F4PrAtFLuS1P7",
-  // "HyUDgtmrERNC6xnPUjxs7fvkB6rX5esqvP5LW4LiXzrV",
+  "6FcJaAzQnuoA6o3sVw1GD6Ba69XuL5jinZpQTzJhd2R3",
+  "Bw77MGpg189EaATjN67WXcnp3c4544LhKoV4Ftmdg4C",
+  "H2H2pJuccdvpET9A75ajB3GgdYdCUL4T3kiwUMA6DJ7q",
+  "Bxk1JQCbVjpeFnjzvH5n9bepnZeHjRADUFwZiVC7L5Gq",
+  "F8rZviSSuqgkTsjMeoyrTUSNSqh7yNDCAozJkxm7eujY",
+  "GrKPvcdHVb4cwR5a2CCgCTvdkSqhNDRgSUiUVzXRWLk6",
+  "5UKRRSxbi4PgPnQU2ZqtukUxd1fyN6ydn1hoxivP46A8",
+  "GAqyPziKPwVpwKaeqPhEsxiy6MwQ2bvtodruWErpLVKo",
+  "2dxJ4eMkhMxm1ZqpAhKsjunvyziuq1JRnuHaqKFRY8et",
+  "FTBLaLcrx1aXALW2UEpu8a6HLRVFATezkK12wCABPAiA",
+  "Dmngi1MDEQU9fm6sX39EuyT3EpYEmXYuyg56uEjVCkD6",
+  "5ZxAjKpbYje5fCxhvnRYxbMh6XSZm5Cd7RA9mMGb1DLY",
+  "7pfaZcAqpWRHpEqGMwPQrn5tj5WVQ48F4PrAtFLuS1P7",
+  "HyUDgtmrERNC6xnPUjxs7fvkB6rX5esqvP5LW4LiXzrV",
 ];
 
 // Case-sensitive
